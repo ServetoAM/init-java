b08a931646e35ed4c1bce805f0243eca72ac3ed5
Serveto Anne-Marie <servetonguyen@outlook.com>
2022-05-12 10:08:16 +0200

    Added teacher's solution and commented mine

--- src/exercice2.java
+++ src/exercice2.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-public class exercice2 {
+/*public class exercice2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -24,5 +24,30 @@ public class exercice2 {
             int calcResult = num * i;
             System.out.println(String.format(result, num, i, calcResult ));
         }
-    }}
+    }}*/
+public class exercice2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Integer number = null;
 
+        while (number == null) {
+            try {
+                System.out.print("Entrez un nombre : ");
+                number = scanner.nextInt(); // Exception potentiellement levée, donc la variable number reste à null
+                scanner.close();
+            } catch (InputMismatchException e) {
+                System.out.println("Attention ! Vous devez entrez un nombre");
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Mince une erreur s'est produite :(");
+                return;
+            }
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(String.format("%d x %d = %d", number, i, number * i));
+        }
+
+
+    }
+}
